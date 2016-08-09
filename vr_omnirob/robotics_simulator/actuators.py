2633e977520f8330ae5985fb197d5b51921d7870
Cristian Axenie <cristian.axenie@gmail.com>
2016-08-09 23:33:08 +0200

    Add vr functionality and full control of robot from nengo.

--- vr_omnirob/robotics_simulator/actuators.py
+++ vr_omnirob/robotics_simulator/actuators.py
@@ -1,32 +1,55 @@
 import vrep
 import ctypes
+import numpy as np
+import math
 
 vrep_mode = vrep.simx_opmode_oneshot
+
 
 # dim = 3
 def position(cid, handle, val):
     err = vrep.simxSetObjectPosition(cid, handle, -1, val, vrep_mode)
 
+
 # dim = 3
 def orientation(cid, handle, val):
     err = vrep.simxSetObjectOrientation(cid, handle, -1, val, vrep_mode)
+
 
 # dim = 1
 def joint_torque(cid, handle, val):
     vrep.simxSetJointTargetVelocity(cid, handle, val*100, vrep_mode)
     vrep.simxSetJointForce(cid, handle, abs(val), vrep_mode)
 
+
+# dim = joints_num
+def joint_positions(cid, handle, val):
+    # This function does not use handle, it just exists in the signature
+    # to make it consistent
+    joint_values = np.zeros(4)
+    for i in range(4):
+       joint_values[i] = val[i]
+    packedData = vrep.simxPackFloats(joint_values.flatten())
+    raw_bytes = (ctypes.c_ubyte * len(packedData)).from_buffer_copy(packedData)
+    err = vrep.simxSetStringSignal(cid, "moveToPosition",
+                                   raw_bytes,
+                                   vrep.simx_opmode_oneshot)
+
+
 # dim = 1
 def joint_velocity(cid, handle, val):
     vrep.simxSetJointTargetVelocity(cid, handle, val, vrep_mode)
+
 
 # dim = 1
 def float_signal(cid, handle, val, signal_name):
     err = vrep.simxSetFloatSignal(cid, signal_name, val, vrep_mode)
 
+
 # dim = 1
 def integer_signal(cid, handle, val, signal_name):
     err = vrep.simxSetIntegerSignal(cid, signal_name, int(val), vrep_mode)
+
 
 # dim = 4
 def quadcopter_rotors(cid, handle, val):
@@ -36,7 +59,7 @@ def quadcopter_rotors(cid, handle, val):
     for i in range(4):
       motor_values[i] = val[i]
     packedData=vrep.simxPackFloats(motor_values.flatten())
-    raw_bytes = (ctypes.c_ubyte * len(packedData)).from_buffer_copy(packedData) 
+    raw_bytes = (ctypes.c_ubyte * len(packedData)).from_buffer_copy(packedData)
     err = vrep.simxSetStringSignal(cid, "rotorTargetVelocities",
                                    raw_bytes,
                                    vrep_mode)
--- vr_omnirob/robotics_simulator/omniarm_motor_babbling.py
+++ vr_omnirob/robotics_simulator/omniarm_motor_babbling.py
@@ -3,6 +3,7 @@
 import nengo
 import sensors
 import actuators
+import math
 from robots import CustomRobot
 from functools import partial
 
@@ -20,31 +21,27 @@ omnibot = CustomRobot(sim_dt=0.05, nengo_dt=0.001, sync=True)
 
 
 # robot wheels
-omnibot.add_actuator("OmniWheel", actuators.joint_torque)
-omnibot.add_actuator("OmniWheel#0", actuators.joint_torque)
-omnibot.add_actuator("OmniWheel#1", actuators.joint_torque)
+omnibot.add_actuator("OmniWheel", actuators.joint_velocity)
+omnibot.add_actuator("OmniWheel#0", actuators.joint_velocity)
+omnibot.add_actuator("OmniWheel#1", actuators.joint_velocity)
 # robot arm
-omnibot.add_actuator("PhantomXPincher_joint2", actuators.joint_torque)
-omnibot.add_actuator("PhantomXPincher_joint3", actuators.joint_torque)
-omnibot.add_actuator("PhantomXPincher_joint4", actuators.joint_torque)
-omnibot.add_actuator("PhantomXPincher_joint5", actuators.joint_torque)
-
+omnibot.add_actuator("PhantomXPincher", actuators.joint_positions,4)
 
 model.config[nengo.Ensemble].neuron_type=nengo.LIF()
 with model:
   # Create a Node that interfaces Nengo with V-REP
   robot = nengo.Node(omnibot, size_in=7, size_out=0)
 
-  motors = nengo.Ensemble(n_neurons=100, dimensions=3, radius=3)
-  servos = nengo.Ensemble(n_neurons=100, dimensions=4, radius=3)
+  motors_population = nengo.Ensemble(n_neurons=100, dimensions=3, radius=3)
+  servos_population = nengo.Ensemble(n_neurons=100, dimensions=4, radius=3)
 
   # Create a single slider to control the speed
-  speed = nengo.Node([0])
-  joints = nengo.Node([0])
+  speed = nengo.Node([0]*3)
+  joints = nengo.Node([0]*4)
 
-  nengo.Connection(speed, motors, transform=[[1],[2],[10]])
-  nengo.Connection(joints, servos,  transform=[[1],[20],[1],[40]])
+  nengo.Connection(speed, motors_population, transform=[10, 10, 10])
+  nengo.Connection(joints, servos_population)
 
 
-  nengo.Connection(motors, robot[1:4])
-  nengo.Connection(servos, robot[3:7])
+  nengo.Connection(motors_population, robot[1:4])
+  nengo.Connection(servos_population, robot[3:7])
