e15d21213e18b5d0ded0c132836382d53091487f
Cristian Axenie <cristian.axenie@tum.de>
2016-11-30 17:16:27 +0100

    Initial test. Stability check to do.

--- omnirob/nstbot/examples/Robot_Goose_Server.sh
+++ omnirob/nstbot/examples/Robot_Goose_Server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# initialize the sensory data forwarding
+./omnirob-port-mirroring-usbip.sh &
+# initialize the memmapped file communication
 nohup python nengo_omnibot_init_arm_dvs.py &
 nohup python nengo_omnibot_init_left_dvs.py &
 nohup python nengo_omnibot_init_right_dvs.py &
 nohup python nengo_omnibot_init.py &
-nohup python tornado_websocket_server.py & 
+# needed ?!?
+# nohup python tornado_websocket_server.py &
